a6625c0c1853a182e6dd61cbcbac5e37dadbab96
Lindsay <lrc3233@gmail.com>
2018-05-29 13:47:23 -0700

    Cadeparade/ch1968/occasionally when you navigate to a build (#560)
    * convert initializeSnapshotOrdering to concurrency task and add a small timeout
    
    * move comment

--- app/controllers/organization/project/builds/build.js
+++ app/controllers/organization/project/builds/build.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import snapshotSort from 'percy-web/lib/snapshot-sort';
 import {snapshotsWithDiffForBrowser} from 'percy-web/lib/filtered-comparisons';
+import {task, timeout} from 'ember-concurrency';
 
 // NOTE: before adding something here, consider adding it to BuildContainer instead.
 // This controller should only be used to maintain the state of which snapshots have been loaded.
@@ -18,17 +19,25 @@ export default Controller.extend({
   // Creates a hash with a keys of each browser id,
   // and the correctly ordered snapshots as the values and sets it as
   // allChangedBrowserSnapshotsSorted.
-  initializeSnapshotOrdering() {
+  _initializeSnapshotOrdering: task(function*() {
     const orderedBrowserSnapshots = {};
-    const browsers = this.get('build.browsers');
     // Get snapshots without making new request
-    const buildSnapshots =
+    const buildSnapshotsWithDiffs =
       this.get('build')
         .hasMany('snapshots')
         .value() || [];
 
+    // If there are no snapshots with changes, this method will complete too fast for
+    // the loading state to display properly. This gives a small delay so we can associate
+    // all the data together and display the build page correctly.
+    if (!buildSnapshotsWithDiffs.get('length')) {
+      yield timeout(300);
+    }
+
+    const browsers = this.get('build.browsers');
+
     browsers.forEach(browser => {
-      const snapshotsWithDiffs = snapshotsWithDiffForBrowser(buildSnapshots, browser);
+      const snapshotsWithDiffs = snapshotsWithDiffForBrowser(buildSnapshotsWithDiffs, browser);
       const sortedSnapshotsWithDiffs = snapshotSort(snapshotsWithDiffs.toArray(), browser);
       const approvedSnapshots = sortedSnapshotsWithDiffs.filterBy('isApprovedByUserEver');
       const unreviewedSnapshots = sortedSnapshotsWithDiffs.filterBy('isUnreviewed');
@@ -42,5 +51,9 @@ export default Controller.extend({
       allChangedBrowserSnapshotsSorted: orderedBrowserSnapshots,
       isSnapshotsLoading: false,
     });
+  }).drop(),
+
+  initializeSnapshotOrdering() {
+    this.get('_initializeSnapshotOrdering').perform();
   },
 });
