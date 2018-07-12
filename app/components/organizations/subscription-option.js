e83c9b9a46163d8032e5488a1971ea4b52738aca
LC <lrc3233@gmail.com>
2018-07-11 17:58:25 -0700

    fix css

--- app/components/organizations/subscription-option.js
+++ app/components/organizations/subscription-option.js
@@ -3,6 +3,7 @@ import {computed} from '@ember/object';
 import {inject as service} from '@ember/service';
 
 export default Component.extend({
+  tagName: '',
   subscriptionData: service(),
   isCustom: computed(function() {
     return this.get('subscriptionData.PLAN_IDS').indexOf(this.get('planData.id')) === -1;
