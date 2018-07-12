eab035e5ab934857e3b99fd120bc6f14f564d081
LC <lrc3233@gmail.com>
2018-07-12 11:29:19 -0700

    simplify template logic in subscription-list-item

--- app/components/organizations/subscription-list-item.js
+++ app/components/organizations/subscription-list-item.js
@@ -8,4 +8,23 @@ export default Component.extend({
   isCustom: computed(function() {
     return this.get('subscriptionData.PLAN_IDS').indexOf(this.get('planData.id')) === -1;
   }),
+  isSelected: computed('plan.id', 'planData.id', function() {
+    return this.get('plan.id') === this.get('planData.id');
+  }),
+
+  selectButtonText: computed('buttonText', 'isSelected', function() {
+    if (this.get('buttonText')) {
+      return this.get('buttonText');
+    } else if (this.get('isSelected')) {
+      return 'Selected Plan';
+    } else {
+      return 'Select Plan';
+    }
+  }),
+
+  actions: {
+    handleSelection() {
+      console.log('choosing one');
+    },
+  },
 });
