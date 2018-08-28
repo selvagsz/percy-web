4f908fdf8d7df1f0b60174ccb6cac5b89a7ef19f
Lindsay <lrc3233@gmail.com>
2018-08-28 09:48:21 -0700

    allow access to users with adminMode (#685)

--- app/lib/is-user-member-of-org.js
+++ app/lib/is-user-member-of-org.js
@@ -1,5 +1,6 @@
 import {resolve} from 'rsvp';
 import DS from 'ember-data';
+import AdminMode from 'percy-web/lib/admin-mode';
 
 // This method _always_ returns a promise.
 // Wherever you use this, you must not make any decisions based on the final value
@@ -7,7 +8,9 @@ import DS from 'ember-data';
 export default function isUserMemberPromise(org) {
   return DS.PromiseObject.create({
     promise: _getOrganizationUsers(org).then(orgUsers => {
-      return orgUsers && !!orgUsers.get('firstObject');
+      const isUserMemberOfOrg = orgUsers && !!orgUsers.get('firstObject');
+      const isUserAdminMode = AdminMode.isAdmin();
+      return isUserMemberOfOrg || isUserAdminMode;
     }),
   });
 }
