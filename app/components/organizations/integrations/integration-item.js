c1be6a3fc306dd41a24fa8d0580f8c66f4d4b66f
Faun <github@faun.me>
2018-08-31 10:03:13 -0700

    Change link location (#692)

--- app/components/organizations/integrations/integration-item.js
+++ app/components/organizations/integrations/integration-item.js
@@ -36,7 +36,7 @@ const INTEGRATIONS_LOOKUP = {
   [GITLAB_SELF_HOSTED_INTEGRATION_TYPE]: {
     textName: 'GitLab Self-Hosted',
     isBeta: true,
-    betaLink: 'https://docs.percy.io/docs/gitlab',
+    betaLink: 'https://docs.percy.io/docs/gitlab-self-hosted',
     iconName: 'gitlab-icon-lg',
     organizationModelAttribute: 'isGitlabSelfHostedIntegrated',
     settingsRouteSlug: 'gitlab-self-hosted',
