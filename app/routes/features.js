bc0508ed5fd137f0b5fa58a793fb9b96a64ece18
Lindsay <lrc3233@gmail.com>
2018-09-06 11:32:40 -0700

    Features page contentful implementation (#707)
    * add adapters, serializers, models for marketing site contentful models
    
    * features page
    
    * add alt text, rename footer property

--- app/routes/features.js
+++ app/routes/features.js
@@ -1,11 +1,33 @@
 import Route from '@ember/routing/route';
 import ResetScrollMixin from '../mixins/reset-scroll';
 import {variation} from 'ember-launch-darkly';
+import {hash} from 'rsvp';
+import MarketingRouteMixin from 'percy-web/mixins/marketing-route';
 
-export default Route.extend(ResetScrollMixin, {
+export default Route.extend(ResetScrollMixin, MarketingRouteMixin, {
   beforeModel() {
     if (!variation('updated-marketing-site')) {
       this.transitionTo('/');
     }
+  },
+
+  pageType: 'Features',
+  footerType: 'Generic',
+
+  model() {
+    return hash({
+      hero: this._getHero(this.get('pageType')),
+      contentBlocks: this._getContentBlocks(this.get('pageType')),
+      footer: this._getFooter(this.get('footerType')),
+    });
+  },
+
+  actions: {
+    didTransition() {
+      this._super.apply(this, arguments);
+
+      // TODO: Add organization tracking
+      this.analytics.track('Features Viewed');
+    },
   },
 });
