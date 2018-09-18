7bc1a19002cba775085c2d27ec7fc5c9b18b4435
Michael Padon <maprules1000@gmail.com>
2018-09-18 08:58:47 -0500

    Make sure time is flagged as UTC before it is used (#727)

--- app/models/usage-stat.js
+++ app/models/usage-stat.js
@@ -13,7 +13,7 @@ export default DS.Model.extend({
       return;
     }
 
-    return dayStats.map(dayStat => [moment(dayStat[0]), dayStat[1]]);
+    return dayStats.map(dayStat => [moment.utc(dayStat[0]), dayStat[1]]);
   }),
 
   // Everything in the API is in cents, but we need to display dollars, so divide by 100.
