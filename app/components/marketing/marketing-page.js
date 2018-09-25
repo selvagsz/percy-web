9bb529dfdab35e6e7eb15eb92be71a080749eea5
Mike Fotinakis <mike@fotinakis.com>
2018-09-24 20:42:05 -0700

    Fix link hijacking. (#768)
    * Fix link hijacking.
    
    * Ignore compound selectors.

--- app/components/marketing/marketing-page.js
+++ app/components/marketing/marketing-page.js
@@ -5,9 +5,10 @@ export default Component.extend({
     // Support third-party relative links that Ember doesn't know about. This emulates the behavior
     // of link-to for smooth in-app transitions, but for links that come from Contenful and rendered
     // Markdown. This prevents full-page refreshes on links that we know we can transition to.
-    if (event.target.tagName === 'A' && event.target.href.startsWith('/')) {
+    if (event.target.tagName === 'A' && event.target.href.startsWith(window.location.origin)) {
       // The target link is part of this app. Hijack the transition and stop bubbling.
-      let path = event.target.href.split('#')[0].slice(1);
+      let path = event.target.href.split(window.location.origin)[1].split('#')[0];
+
       this.send('navigate', path);
       return false;
     }
