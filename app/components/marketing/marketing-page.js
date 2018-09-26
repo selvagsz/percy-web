20e5940daa24f322a74be74c9683376c930d217e
Lindsay <lrc3233@gmail.com>
2018-09-26 14:52:54 -0700

    allow cmd/ctrl click to open links in new tab (#783)

--- app/components/marketing/marketing-page.js
+++ app/components/marketing/marketing-page.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  transitionTo: null,
+
   click(event) {
     // Support third-party relative links that Ember doesn't know about. This emulates the behavior
     // of link-to for smooth in-app transitions, but for links that come from Contenful and rendered
@@ -9,14 +11,13 @@ export default Component.extend({
       // The target link is part of this app. Hijack the transition and stop bubbling.
       let path = event.target.href.split(window.location.origin)[1].split('#')[0];
 
-      this.send('navigate', path);
-      return false;
+      // If ctrl or cmd are pressed, open a new tab.
+      if (event.ctrlKey || event.metaKey) {
+        return true;
+      } else {
+        this.get('transitionTo')(path);
+        return false;
+      }
     }
-  },
-
-  actions: {
-    navigate(path) {
-      this.get('transitionTo')(path);
-    },
   },
 });
