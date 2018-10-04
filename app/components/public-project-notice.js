66dcead11de1a4fe57c191883990ef8292a9d7b3
Lindsay <lrc3233@gmail.com>
2018-10-04 17:41:42 +0200

    Update PVP banner styling. (#791)
    * update styles and org name
    
    * remove reference to organization or specific project/build object
    
    * sprinkle some responsive on it

--- app/components/public-project-notice.js
+++ app/components/public-project-notice.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import {inject as service} from '@ember/service';
 import {readOnly} from '@ember/object/computed';
+import {computed} from '@ember/object';
 import EnsureStatefulLogin from 'percy-web/mixins/ensure-stateful-login';
 import {getOwner} from '@ember/application';
 import localStorageProxy from 'percy-web/lib/localstorage';
@@ -9,6 +10,22 @@ import {AUTH_REDIRECT_LOCALSTORAGE_KEY} from 'percy-web/router';
 export default Component.extend(EnsureStatefulLogin, {
   session: service(),
   currentUser: readOnly('session.currentUser'),
+
+  displayText: computed('currentUser', function() {
+    if (this.get('currentUser')) {
+      return 'You’re viewing a public Percy project. Only members of this organization can approve snapshots.';  // eslint-disable-line
+    } else {
+      return 'Welcome to Percy! Sign in to approve snapshots or create an account to start a free trial.'  // eslint-disable-line
+    }
+  }),
+
+  btnText: computed('currentUser', function() {
+    if (this.get('currentUser')) {
+      return 'Jump to your projects';
+    } else {
+      return 'Sign in or start trial';
+    }
+  }),
 
   actions: {
     showLoginModal() {
