bf0c683ed379b3b7e3cc4988d28e18ae8ab02f09
Paul Friedman <paul@percy.io>
2018-10-10 12:35:15 -0700

    wip

--- app/routes/organization/project/webhooks/webhook-config.js
+++ app/routes/organization/project/webhooks/webhook-config.js
@@ -2,8 +2,11 @@ import {hash} from 'rsvp';
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import ResetScrollMixin from 'percy-web/mixins/reset-scroll';
+import {inject as service} from '@ember/service';
 
 export default Route.extend(AuthenticatedRouteMixin, ResetScrollMixin, {
+  flashMessages: service(),
+
   model(params) {
     const webhookConfig = this.store.findRecord('webhookConfig', params.webhook_config_id);
     const project = this.modelFor('organization.project');
@@ -11,5 +14,12 @@ export default Route.extend(AuthenticatedRouteMixin, ResetScrollMixin, {
     const projects = this.store.query('project', {organization: organization});
 
     return hash({organization, project, projects, webhookConfig});
+  },
+
+  actions: {
+    webhookConfigUpdated() {
+      this.get('flashMessages').success('u did it :) 🦄');
+      window.scrollTo(0, 0);
+    },
   },
 });
