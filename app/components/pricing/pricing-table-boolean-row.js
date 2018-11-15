e4c350e5da4a00908bab477aab6771beed10d5a8
Lindsay <lrc3233@gmail.com>
2018-11-14 17:42:35 -0800

    Compare string to string, not to boolean (#900)
    * compare string to string, not to boolean
    
    * check for both boolean and text values of true

--- app/components/pricing/pricing-table-boolean-row.js
+++ app/components/pricing/pricing-table-boolean-row.js
@@ -1,5 +1,30 @@
 import Component from '@ember/component';
+import {computed} from '@ember/object';
+import {readOnly} from '@ember/object/computed';
 
 export default Component.extend({
   tagName: 'tr',
+  field: null,
+
+  _firstStartupPlan: readOnly('plans.startupPlans.firstObject'),
+  _businessPlan: readOnly('plans.businessPlan'),
+  _enterprisePlan: readOnly('plans.enterprisePlan'),
+
+  isStartupPlansTruthy: computed('_firstStartupPlan', function() {
+    return this._isTruthy('_firstStartupPlan');
+  }),
+
+  isBusinessPlanTruthy: computed('_businessPlan', function() {
+    return this._isTruthy('_businessPlan');
+  }),
+
+  isEnterprisePlanTruthy: computed('_enterprisePlan', function() {
+    return this._isTruthy('_enterprisePlan');
+  }),
+
+  _isTruthy(plan) {
+    const field = this.get('field');
+    const value = this.get(plan).get(field);
+    return value === 'true' || value === true;
+  },
 });
