cf3e889828d42080883a28374d5c1806aba9aa48
Rylee <rylee@percy.io>
2018-11-26 11:51:00 -0700

    WIP- Move invite form into header

--- app/components/organizations/users-header.js
+++ app/components/organizations/users-header.js
@@ -7,4 +7,10 @@ export default Component.extend({
     const onInvite = this.get('isInvitePath');
     return onInvite || !isAdmin;
   }),
+  showInviteForm: computed('isInvitePath', 'organization.currentUserIsAdmin', function() {
+    const isAdmin = this.get('organization.currentUserIsAdmin');
+    const onInvite = this.get('isInvitePath');
+    const canInvite = this.get('organization.canInvite');
+    return onInvite && isAdmin && canInvite;
+  }),
 });
