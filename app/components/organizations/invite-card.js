ead679ad9801412a0ddb0a73cfee6770c8b7fb5d
Rylee <rylee@percy.io>
2018-11-26 17:02:05 -0700

    Reload organization (for updated seat totals) when an invite is cancelled

--- app/components/organizations/invite-card.js
+++ app/components/organizations/invite-card.js
@@ -1,18 +1,21 @@
 import Component from '@ember/component';
 import {alias} from '@ember/object/computed';
 import {computed} from '@ember/object';
+import {inject as service} from '@ember/service';
 
 export default Component.extend({
   defaultAvatar: computed(function() {
     return `${window.location.origin}/images/placeholder-avatar.jpg`;
   }),
   isAdmin: alias('organization.currentUserIsAdmin'),
+  store: service(),
   actions: {
     onCopyInviteUrlToClipboard() {
       this.get('flashMessages').success('Invite URL was copied to your clipboard');
     },
     cancelInvite() {
       const invite = this.get('invite');
+      const orgId = this.get('organization').get('id');
       let confirmation = confirm(
         `Are you sure you want to cancel the invitation to ${invite.email}?`,
       );
@@ -29,6 +32,9 @@ export default Component.extend({
             `There was a problem cancelling the invitation to ${invite.email}.` +
               ' Please try again or contact customer support.',
           );
+        })
+        .finally(() => {
+          this.get('store').findRecord('organization', orgId, {reload: true});
         });
     },
   },
